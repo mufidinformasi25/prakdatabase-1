9056fd5fb575c91ebf400d9539cc358347e4b4df
Hilmi Mufid <hilmimufid21@students.untidar.ac.id>
2026-02-17 23:11:41 +0700

    Add files via upload

---
+++ laprak1.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 17 Feb 2026 pada 15.20
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `laprak1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `nama_table`
+--
+
+CREATE TABLE `nama_table` (
+  `NPM` char(9) NOT NULL,
+  `NAMA` varchar(22) NOT NULL,
+  `Tempat_Lahir` varchar(20) NOT NULL,
+  `Tanggal_Lahir` date NOT NULL,
+  `No_HP` varchar(25) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `nama_table`
+--
+ALTER TABLE `nama_table`
+  ADD PRIMARY KEY (`NPM`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
